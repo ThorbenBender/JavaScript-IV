15a4b6ca94fbc3f083db775d6c8f601baf20b07d
Thorben Bender <bender.thorben0@gmail.com>
2019-01-24 15:30:30 +0100

    mvp done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -46,3 +46,87 @@ class Student extends Person {
     }
 }
 
+class ProjectManager extends Instructor {
+    constructor(attributes){
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy time!`);
+    }
+}
+
+const Mark = new Instructor({
+    name: 'Mark',
+    age: 20,
+    location: 'Paris',
+    gender: 'Male',
+    specialty: 'React',
+    favLanguage: 'Python',
+    catchPhrase: 'If you want more ram, just download it.'
+});
+
+const Sean = new Instructor({
+    name: 'Sean',
+    age: 26,
+    location: 'Tokyo',
+    gender: 'Male',
+    specialty: '',
+    favLanguage: 'C++',
+    catchPhrase: 'Did you try to restart your pc?'
+});
+
+const John = new Instructor({
+    name: 'John',
+    age: 32,
+    location: 'Seoul',
+    gender: 'Female',
+    specialty: 'Redux',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'If you want more ram, just download it.'
+});
+
+const Tim = new Student({
+    name: 'Tim',
+    age: 26,
+    location: 'Berlin',
+    gender: 'Male',
+    previousBackground: 'owned a own company',
+    className: 'WEBEU1',
+    favSubjects: ['Less', 'React', 'Redux']
+});
+
+const Max = new Student({
+    name: 'Max',
+    age: 32,
+    location: 'Dublin',
+    gender: 'Female',
+    previousBackground: 'Studied economics',
+    className: 'WEBEU1',
+    favSubjects: ['CSS', 'HTML5', 'React']
+});
+
+const Samar = new ProjectManager({
+    name: 'Samar',
+    age: 32,
+    location: 'Dheli',
+    gender: 'Male',
+    specialty: 'React',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'If you have windows, you\'re doomed',
+    gradClassName: 'WEBEU1',
+    favInstructor: 'Gabriel'
+});
+
+const Oliver = new ProjectManager({
+    name: 'Oliver',
+    age: 32,
+    location: 'London',
+    gender: 'Male',
+    specialty: 'Python',
+    favLanguage: 'C++',
+    catchPhrase: 'If you have apple, you\'re doomed',
+    gradClassName: 'WEBEU1',
+    favInstructor: 'Tom'
+});
